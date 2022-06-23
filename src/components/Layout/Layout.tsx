03d49877139d279e46bffc6280bd8ab9263a14d2
DESKTOP-OPG6A2A\hp <shahidmmed@gmail.com>
2022-06-23 08:08:26 +0000

    layout, theme and navbar updates

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import ThemeToggler from "../Theme/ThemeToggler";
 import "./Layout.scss";
 
 const Layout: React.FC = () => {
   return (
     <div className="main-container" data-uk-height-viewport="expand: true">
       <Navbar />
-      <Outlet />
+      <Outlet /> <ThemeToggler />
     </div>
   );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
 import { HashLink as Link } from "react-router-hash-link";
 import menu from "../../assets/icons/menu.svg";
 
 const Navbar: React.FC = () => {
+  const [toggled, setToggled] = useState(false);
   useEffect(() => {}, []);
+
+  const showNav = () => {
+    const primaryNav = document.querySelector(".primary-nav");
+    const navToggle = document.querySelector(".toggle-mobile-nav");
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (!toggled) {
+      document.body.classList.add("scroll-lock");
+    } else {
+      document.body.classList.remove("scroll-lock");
+    }
+    setToggled(!toggled);
+
+    if (visibility === "false") {
+      primaryNav.setAttribute("data-visible", "true");
+      navToggle.setAttribute("aria-expanded", "true");
+    } else if (visibility === "true") {
+      primaryNav.setAttribute("data-visible", "false");
+      navToggle.setAttribute("aria-expanded", "false");
+    }
+
+    primaryNav.addEventListener("focusout", () => {
+      console.log("clic");
+    });
+  };
+
   return (
     <div
       data-uk-sticky="start: 200; animation: uk-animation-slide-top; sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; cls-inactive: uk-navbar-transparent uk-light"
-      className="top-nav uk-dark"
+      className="top-nav"
     >
       <nav className="uk-navbar-container uk-navbar-transparent uk-padding-small uk-navbar nav-section">
         <div className="uk-navbar-left">
@@ -21,10 +48,15 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="uk-navbar-right">
-          <button aria-controls="primary-nav" className="toggle-mobile-nav">
+          <button
+            aria-controls="primary-nav"
+            aria-expanded="false"
+            className="toggle-mobile-nav"
+            onClick={showNav}
+          >
             <span className="sr-only"></span>
           </button>
-          <ul className="uk-navbar-nav uk-animation-slide-top primary-nav">
+          <ul className="uk-navbar-nav primary-nav" data-visible="false">
             <li>
               <Link className="uk-navbar-item" to="/#about">
                 About
