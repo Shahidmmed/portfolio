c4f8cb20202cf86c2e4be6a45ea2fad22017fa7b
DESKTOP-OPG6A2A\hp <shahidmmed@gmail.com>
2022-07-08 20:06:32 +0000

    page updates

--- src/components/Sections/Home.tsx
+++ src/components/Sections/Home.tsx
@@ -3,12 +3,14 @@ import "./Styles.scss";
 const Home: React.FC = () => {
   return (
     <>
-      <div className="uk-section section" id="home">
+      <div className="uk-section section uk-margin-large" id="home">
         <div className="uk-container uk-container-small">
           <h3 className="accentText">Hi, my name is </h3>
           <div className="content">
             <h2 className="hero uk-margin-remove-top">Abdul Shahid,</h2>
-            <h2 className="heading uk-margin-remove-top">Software Engineer</h2>
+            <h2 className="profession uk-margin-remove-top">
+              Software Engineer
+            </h2>
           </div>
           <p></p>
         </div>
