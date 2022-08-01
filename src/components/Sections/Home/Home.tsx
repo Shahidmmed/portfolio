f893e44390c6ac8398845baee3313993cd89d21f
DESKTOP-OPG6A2A\hp <shahidmmed@gmail.com>
2022-08-01 07:27:16 +0000

    font size fixes and test animation

--- src/components/Sections/Home/Home.tsx
+++ src/components/Sections/Home/Home.tsx
@@ -8,7 +8,6 @@ const Home: React.FC = () => {
           <h2 className="hero uk-margin-remove-top">Abdul Shahid</h2>
           <h2 className="profession uk-margin-remove-top">Software Engineer</h2>
         </div>
-        <p></p>
       </div>
     </div>
   );
