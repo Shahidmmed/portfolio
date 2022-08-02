b3131bf8d865822f29fced8765633981d145e845
DESKTOP-OPG6A2A\hp <shahidmmed@gmail.com>
2022-08-02 09:31:09 +0000

    logo or image update

--- src/components/Sections/Home/Home.tsx
+++ src/components/Sections/Home/Home.tsx
@@ -4,9 +4,16 @@ const Home: React.FC = () => {
   return (
     <div className="uk-section section uk-margin-large home" id="home">
       <div className="uk-container uk-container-small">
-        <div className="content">
-          <h2 className="hero uk-margin-remove-top">Abdul Shahid</h2>
-          <h2 className="profession uk-margin-remove-top">Software Engineer</h2>
+        <div className="content uk-flex">
+          <div>
+            <h2 className="hero uk-margin-remove-top">Abdul Shahid</h2>
+            <h2 className="profession uk-margin-remove-top">
+              Software Engineer
+            </h2>
+          </div>
+          <div className="img-content">
+            <div className="logo"></div>
+          </div>
         </div>
       </div>
     </div>
